2ea13e1b91f4a9fa993fcb8cf208f926bd1d4f06
Nikita <nikita27388372@gmail.com>
2025-01-14 10:12:43 +0300

    add open&close methods

--- TimeWaster.Core/IIntervalsRepository.cs
+++ TimeWaster.Core/IIntervalsRepository.cs
@@ -7,6 +7,8 @@ public interface IIntervalsRepository
     IEnumerable<Interval> GetAll();
     Interval? Get(Guid id);
     IEnumerable<Interval> GetByUser(Guid userId);
+    IEnumerable<Interval> GetByUsersInDate(Guid userId, DateOnly date);
+    Interval? GetOpen(Guid userId);
     Interval? Create(Interval interval);
     Interval? Update(Interval interval);
     void Delete(Guid id);
