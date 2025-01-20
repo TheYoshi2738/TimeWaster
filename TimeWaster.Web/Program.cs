d45f33bd893b21b03981502f8165ac68b7c28705
Nikita <nikita27388372@gmail.com>
2025-01-20 08:45:37 +0300

    dateTime converter to UTC for DB

--- TimeWaster.Web/Program.cs
+++ TimeWaster.Web/Program.cs
@@ -4,6 +4,7 @@ using TimeWaster.Core.Services;
 using TimeWaster.Data;
 using TimeWaster.Data.Intervals.Repositories;
 using TimeWaster.Data.Users.Repositories;
+using TimeWaster.Web.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,10 @@ builder.Services.AddScoped<IIntervalsRepository, IntervalsPostgreSqlRepository>(
 builder.Services.AddScoped<IUsersRepository, UsersPostgreSqlRepository>();
 builder.Services.AddScoped<IntervalService>();
 builder.Services.AddScoped<UserService>();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new DateTimeToUtcConverter());
+} );
 
 var app = builder.Build();
 
---
+++ TimeWaster.Web/Utils/DateTimeToUtcConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TimeWaster.Web.Utils;
+
+public class DateTimeToUtcConverter : JsonConverter<DateTime>
+{
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var dateTime = DateTime.Parse(reader.GetString() ?? string.Empty);
+        return dateTime.Kind == DateTimeKind.Utc ? dateTime : dateTime.ToUniversalTime();
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToUniversalTime().ToString("o"));
+    }
+}
